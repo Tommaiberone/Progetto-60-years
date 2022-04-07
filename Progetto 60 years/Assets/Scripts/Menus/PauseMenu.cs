df7d5052767373ceea4bb5a1bbae8bca4316de86
Tommaiberone <tommasobersani@gmail.com>
2022-04-07 18:50:18 +0200

    Bozza sistema Menu

---
+++ Progetto 60 years/Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public static bool MenuIsActive = false;
+
+    public GameObject pauseMenuUI;
+    public GameObject OptionsMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!GameIsPaused)
+                pause();
+
+            else if (GameIsPaused && !MenuIsActive)
+                resume();
+
+            else if (GameIsPaused && MenuIsActive)
+            {
+                OptionsMenuUI.SetActive(false);
+                pauseMenuUI.SetActive(true);
+                setMenuActive();
+            }
+                
+        }
+
+    }
+    
+    public void resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+
+    public void pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+
+    }
+
+    public void quitToMainMenu()
+    {
+        GameIsPaused = false;
+        MenuIsActive = false;
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(0);
+    }
+
+    public void setMenuActive()
+    {
+        if (MenuIsActive)
+            MenuIsActive = false;
+        else
+            MenuIsActive = true;
+    }
+}
